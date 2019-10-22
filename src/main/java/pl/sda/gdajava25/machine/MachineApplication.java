9940d4d10fb3e73e2881fb1261fa1b6f0a839a24
Filip Trela <ftrela@gmail.com>
2019-10-22 18:39:58 +0200

    first commit

---
+++ src/main/java/pl/sda/gdajava25/machine/MachineApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.gdajava25.machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MachineApplication.class, args);
+    }
+
+}
